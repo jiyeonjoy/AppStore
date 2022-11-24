b9400835ee81437332baeacb60f1d92a53251969
최지연 <choi02647@naver.com>
2022-11-24 13:48:00 +0900

    added TodayCollectionViewCell UI

--- AppStore/Scenes/Today/TodayCollectionViewCell.swift
+++ AppStore/Scenes/Today/TodayCollectionViewCell.swift
@@ -10,7 +10,74 @@ import Kingfisher
 import UIKit
 
 final class TodayCollectionViewCell: UICollectionViewCell {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        label.textColor = .white
+
+        return label
+    }()
+
+    private lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+
+        return label
+    }()
+
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+
+        return label
+    }()
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true // 이거 넣어줘야 이미지가 밖으로 안삐져 나온다고.
+        imageView.layer.cornerRadius = 12.0
+        imageView.backgroundColor = .gray
+
+        return imageView
+    }()
+    
     func setup() {
-        backgroundColor = .orange
+        setupSubViews()
+        
+        subTitleLabel.text = "서브 타이틀"
+        descriptionLabel.text = "설명 설명"
+        titleLabel.text = "메인 타이틀"
+    }
+}
+
+private extension TodayCollectionViewCell {
+    func setupSubViews() {
+        [imageView, titleLabel, subTitleLabel, descriptionLabel]
+            .forEach { addSubview($0) }
+
+        subTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.top.equalToSuperview().inset(24.0)
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(subTitleLabel)
+            $0.trailing.equalTo(subTitleLabel)
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(4.0)
+        }
+
+        descriptionLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.bottom.equalToSuperview().inset(24.0)
+        }
+
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
