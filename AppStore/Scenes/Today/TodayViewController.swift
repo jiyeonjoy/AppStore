6dbb8c568fd9adab74d4db91d9fbb99a22a7d84d
최지연 <choi02647@naver.com>
2022-11-24 11:32:14 +0900

    added TabBarController

---
+++ AppStore/Scenes/Today/TodayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TodayViewController.swift
+//  AppStore
+//
+//  Created by Jiyeon Choi on 2022. 11. 23..
+//
+
+import SnapKit
+import UIKit
+
+final class TodayViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
