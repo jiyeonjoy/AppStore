7dbfcaa782ea7baffafdb57e7f3fb6bfb62548d4
최지연 <choi02647@naver.com>
2022-11-24 13:49:20 +0900

    그림자 추가

--- AppStore/Scenes/Today/TodayCollectionViewCell.swift
+++ AppStore/Scenes/Today/TodayCollectionViewCell.swift
@@ -47,6 +47,10 @@ final class TodayCollectionViewCell: UICollectionViewCell {
     func setup() {
         setupSubViews()
         
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowRadius = 10
+        
         subTitleLabel.text = "서브 타이틀"
         descriptionLabel.text = "설명 설명"
         titleLabel.text = "메인 타이틀"
