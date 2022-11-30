73d25cbeacb1dc5ee5e2ae3738f8cadd294278c6
최지연 <choi02647@naver.com>
2022-11-30 18:32:26 +0900

    feature 추가

--- AppStore/Scenes/App/FeatureSectionView.swift
+++ AppStore/Scenes/App/FeatureSectionView.swift
@@ -9,6 +9,8 @@ import SnapKit
 import UIKit
 
 final class FeatureSectionView: UIView {
+    private var featureList: [Feature] = []
+
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -33,6 +35,9 @@ final class FeatureSectionView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
+        
+        fetchData()
+        collectionView.reloadData()
     }
 
     required init?(coder: NSCoder) {
@@ -42,13 +47,14 @@ final class FeatureSectionView: UIView {
 
 extension FeatureSectionView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        featureList.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeatureSectionCollectionViewCell", for: indexPath) as? FeatureSectionCollectionViewCell
-        cell?.setup()
-        
+        let feature = featureList[indexPath.item]
+        cell?.setup(feature: feature)
+
         return cell ?? UICollectionViewCell()
     }
 }
@@ -73,7 +79,7 @@ private extension FeatureSectionView {
             collectionView,
             separatorView
         ].forEach { addSubview($0) }
-
+        
         collectionView.snp.makeConstraints {
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
@@ -88,5 +94,15 @@ private extension FeatureSectionView {
             $0.top.equalTo(collectionView.snp.bottom).offset(16.0)
             $0.bottom.equalToSuperview()
         }
+    }
+    
+    func fetchData() {
+        guard let url = Bundle.main.url(forResource: "Feature", withExtension: "plist") else { return }
+
+        do {
+            let data = try Data(contentsOf: url)
+            let result = try PropertyListDecoder().decode([Feature].self, from: data)
+            featureList = result
+        } catch {}
     }
 }
--- AppStore/Scenes/AppDetailViewController.swift
+++ AppStore/Scenes/AppDetailViewController.swift
@@ -9,6 +9,7 @@ import SnapKit
 import UIKit
 
 final class AppDetailViewController: UIViewController {
+    private let today: Today
 
     private let appIconImageView: UIImageView = {
         let imageView = UIImageView()
@@ -53,6 +54,16 @@ final class AppDetailViewController: UIViewController {
         return button
     }()
     
+    init(today: Today) {
+        self.today = today
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -61,6 +72,8 @@ final class AppDetailViewController: UIViewController {
         setupViews()
 
         appIconImageView.backgroundColor = .lightGray
+        titleLabel.text = today.title
+        subTitleLabel.text = today.subTitle
     }
 }
 
